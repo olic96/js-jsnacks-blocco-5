5442c0c21a9f1c495ebd4e07688c0be6d104deb7
olic96 <francoolich@gmail.com>
2022-05-16 12:30:09 +0200

    first commit

---
+++ snack 1/main.js
@@ -0,0 +1,67 @@
+const zucchine = [];
+
+const weightZucchine = 0;
+
+let zucchinaUno = {
+    color: "green", 
+    weight: "200g",
+    lengt: "35cm"
+};
+
+let zucchinaDue = {
+    color: "green", 
+    weight: "210g",
+    lengt: "52cm"
+};
+
+let zucchinaTre = {
+    color: "green", 
+    weight: "450g",
+    lengt: "90cm"
+};
+
+let zucchinaQuattro = {
+    color: "green", 
+    weight: "400g",
+    lengt: "90cm"
+};
+
+let zucchinaCinque = {
+    color: "green", 
+    weight: "20g",
+    lengt: "15cm"
+};
+
+let zucchinaSei = {
+    color: "green", 
+    weight: "100g",
+    lengt: "25cm"
+};
+
+let zucchinaSette = {
+    color: "green", 
+    weight: "220g",
+    lengt: "52cm"
+};
+
+let zucchinaOtto = {
+    color: "green", 
+    weight: "200g",
+    lengt: "50cm"
+};
+
+let zucchinaNove = {
+    color: "green", 
+    weight: "200g",
+    lengt: "20cm"
+};
+
+let zucchinaDieci = {
+    color: "green", 
+    weight: "300g",
+    lengt: "28cm"
+};
+
+zucchine.push(zucchinaUno, zucchinaDue, zucchinaTre, zucchinaQuattro, zucchinaCinque, zucchinaSei, zucchinaSette, zucchinaOtto, zucchinaNove, zucchinaDieci);
+
+console.log(zucchine);
